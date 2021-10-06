f2396fa6f4ef7e8886f8a30ef1d4fa34234b24d4
Sp1r14ual <svc6333@gmail.com>
2021-10-06 19:43:24 +0700

    implemented task 15

---
+++ task15.py
@@ -0,0 +1,9 @@
+centimetres = float(input())
+inches = centimetres / 2.54
+feet = inches / 12 
+yards = feet / 3
+miles = yards / 1760
+print(yards,"ярдов")
+print(miles,"мили")
+print(feet,"футов")
+print(inches,"дюймов")
