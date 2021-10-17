324656b5ea45928aa91fe7a45cbda2845caac2fc
Sp1r14ual <svc6333@gmail.com>
2021-10-17 17:31:03 +0700

    Implemented task12.py

---
+++ task12.py
@@ -0,0 +1,6 @@
+total_sum = int(input())
+silver_watches = 96
+golden_watches = 96 / 16
+silver_watches_sum = 48 * 96
+golden_watches_price = (total_sum - silver_watches_sum) / golden_watches
+print(golden_watches_price)
