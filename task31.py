dc16535deedcb76aa5ed9516557d4a12cec2112e
Sp1r14ual <svc6333@gmail.com>
2021-10-24 11:12:38 +0700

    Implemented working solution for task 31

--- task31.py
+++ task31.py
@@ -1,43 +1,8 @@
-def hare(n, calls, recursion_count):
-    if calls == 1:
-        print("(\___/)", end="")
-        if recursion_count < n - 1:
-            print(" ", end="")
-            hare(n, calls, recursion_count + 1)
-        else:
-            print()
-            return
-        
-    if calls == 2:
-        print("(='.'=)", end="")
-        if recursion_count < n - 1:
-            print(" ", end="")
-            hare(n, calls, recursion_count + 1)
-        else:
-            print()
-            return
-        
-    if calls == 3:
-        print('(")_(")', end="")
-        if recursion_count < n - 1:
-            print(" ", end="")
-            hare(n, calls, recursion_count + 1)
-        else:
-            print()
-            return
-        
+top = "(\___/) "
+middle = "(='.'=) "
+bottom = '(")_(") '
 
-
-function_call_number = 0
 n = int(input())
-
-function_call_number += 1
-hare(n, function_call_number, 0)
-
-function_call_number += 1
-hare(n, function_call_number, 0)
-
-function_call_number += 1
-hare(n, function_call_number, 0)
-
-#А что не так? Вы сами мне запретили использовать циклы
+print(top * n)
+print(middle * n)
+print(bottom * n)
