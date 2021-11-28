920c0ff1addbc7e2f790fecff75f1db14870dbce
Sp1r14ual <svc6333@gmail.com>
2021-11-28 14:26:10 +0700

    Implemented task02.py for chapter 2

---
+++ Chapter 2/Exercises/task02.py
@@ -0,0 +1,4 @@
+print("Какие два слова передал первой радиограммой Александр Попов?")
+first_word = input("Первое слово: ")
+second_word = input("Второе слово: ")
+print("Верно" if first_word == "Генрих" and second_word == "Герц" else "Неверно")
